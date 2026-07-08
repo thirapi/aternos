7de744a67780607817d166ab26ed44369c8331a9
Thirapi <thirapi@proton.me>
2026-07-09 04:22:44 +0700

    feat: add Host, Referer, and Origin headers to fetch request

--- worker/index.js
+++ worker/index.js
@@ -4,10 +4,13 @@ export default {
     const target = new URL('https://aternos.org' + url.pathname + url.search)
     const headers = new Headers(request.headers)
 
+    headers.set('Host', 'aternos.org')
     headers.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
     headers.set('Accept', '*/*')
     headers.set('Accept-Language', 'en-US,en;q=0.9')
     headers.set('Accept-Encoding', 'gzip, deflate, br, zstd')
+    headers.set('Referer', 'https://aternos.org/')
+    headers.set('Origin', 'https://aternos.org')
 
     const clientHeaders = ['sec-ch-ua', 'sec-ch-ua-mobile', 'sec-ch-ua-platform',
       'sec-ch-ua-platform-version', 'sec-ch-ua-arch', 'sec-ch-ua-bitness',
